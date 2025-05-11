a07ef56466c16272ac483dfe512b63de6671ca03
Julien Huang <julien.h.dev@gmail.com>
2025-05-11 04:08:33 +0200

    chore(playground): add plugins

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,16 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import {vueServerComponentsPlugin} from '../src/vite/chunk'
+
+const { client, server } = vueServerComponentsPlugin({
+  include: [
+    './src/components/HelloWorld.vue',
+  ]
+})
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    client, server
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    minify: false,
+  }
 })
