6951662c615ba0feb9ece3dcd62868293680206b
Julien Huang <julien.h.dev@gmail.com>
2025-05-12 14:48:44 +0200

    chore: set server entry in playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import {vueServerComponentsPlugin} from '../src/vite/chunk'
