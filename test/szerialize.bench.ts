cd7406a33299e35dd6df7e08c680ec50039b3480
Julien Huang <julien.h.dev@gmail.com>
2025-06-22 23:01:00 +0200

    test: add benchmark between old and new serialize

---
+++ test/szerialize.bench.ts
@@ -0,0 +1,16 @@
+import { bench, describe } from "vitest";
+
+ import { serializeComponent } from "../src/runtime/serializeAsync";
+import { serializeComponent as oldSerialize } from "../src/runtime/serialize";
+import WithAsyncComponent from "virtual:vsc:./fixtures/components/WithAsyncComponent.vue";
+
+
+describe('bench', () => {
+    bench('serialize -- v2', async () => {
+        await serializeComponent(WithAsyncComponent);
+    })
+
+    bench('serialize v1', async () => {
+        await oldSerialize(WithAsyncComponent);
+    })
+})
