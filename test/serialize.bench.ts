1c35e56c92aac5f65336d1c749672f4c9d69e228
Julien Huang <julien.h.dev@gmail.com>
2025-07-09 20:34:41 +0200

    test: remove old impl

--- test/serialize.bench.ts
+++ test/serialize.bench.ts
@@ -1,15 +1,10 @@
 import { bench, describe } from "vitest";
 
 import { serializeComponent } from "../src/runtime/serialize";
-import { serializeComponent as oldSerialize } from "../src/runtime/serialize";
 import WithAsyncComponent from "virtual:vsc:./fixtures/components/WithAsyncComponent.vue";
 
 describe("bench", () => {
-  bench("serialize -- v2", async () => {
+  bench("serialize", async () => {
     await serializeComponent(WithAsyncComponent);
-  });
-
-  bench("serialize v1", async () => {
-    await oldSerialize(WithAsyncComponent);
   });
 });
